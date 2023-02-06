83abef041b0d87adc3d6a80de29a1c75527ee165
Ebenezhar <ebenezharselvakumar@gmail.com>
2023-02-06 15:27:24 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client'
+const socket = io.connect('http://localhost:5000')
 
 function App() {
+  const [data, setData] = useState({ input1: '000', input2: '000', input3: '000', input4: '000' })
+  let spanStyle = {
+    "margin": "10px",
+    "padding": "10px",
+    "border": "1px solid black",
+    "border-radius": "5px",
+  }
+
+  useEffect(() => {
+    socket.on('receive_values', (data) => {
+      setData(data);
+    })
+  }, [socket])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>values</h3>
+      <span style={spanStyle}>{data.input1}</span>
+      <span style={spanStyle}>{data.input2}</span>
+      <span style={spanStyle}>{data.input3}</span>
+      <span style={spanStyle}>{data.input4}</span>
     </div>
   );
 }
